fd25734fb36d2ed037c178be20f917a7c956e3af
xukun <1158674366@qq.com>
2016-12-30 14:22:38 +0800

    Initial Commit

---
+++ nihaoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nihaoTest
+//
+//  Created by xukun on 2016/12/30.
+//  Copyright © 2016年 xukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
